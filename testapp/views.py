4fe3b13b9e95f022de3e4470b55a1f7941e5af0b
naki <nakikun0125@icloud.com>
2024-05-23 20:31:26 +0900

    developing question.html max

--- testapp/views.py
+++ testapp/views.py
@@ -11,48 +11,53 @@ En = []
 for word in mydict:
     En.append(word)
 print(En)
+
+
 @app.route('/')
 def index():
     return render_template('testapp/index.html')
+
 
 @app.route('/add', methods=["get"])
 def addword():
     return render_template('testapp/add.html')
 
-@app.route('/added',methods=["POST"])
+
+@app.route('/added', methods=["POST"])
 def wordadded():
     datas = []
-    data = request.form['data1']    #POSTされたデータを読み込む
-    datas = data.split(' ')         #英語と日本語でセパレート
+    data = request.form['data1']  # POSTされたデータを読み込む
+    datas = data.split(' ')  # 英語と日本語でセパレート
     mydict[datas[0]] = datas[1]
     saver(mydict)
     return render_template('testapp/added.html')
 
+
 @app.route('/check')
 def checkword():
     mydict = reader()
-    return render_template('testapp/check.html',mydict = mydict, En = En)
+    return render_template('testapp/check.html', mydict=mydict, En=En)
 
-@app.route('/question')
+
+@app.route('/question', methods=["POST", "GET"])
 def solvequestions():
+    maxNum = len(En)
+    print(maxNum)
+    return render_template('testapp/question.html', max=maxNum)
 
-    return render_template('testapp/question.html')
 
-@app.route('/rand_q',methods=["POST"])
+@app.route('/rand_q', methods=["POST", "GET"])
 def solving():
-    global num
-    num = request.form['data2']
+    num = request.form['questionNum']
     print(num)
-    question = random_q(mydict)
-    global glb_q
-    glb_q = question
-    return render_template('testapp/solving.html',question = question)
+    return render_template('testapp/solving.html', num=num, dictData=mydict)
+
 
 @app.route('/TorF', methods=['post'])
 def Check_ans():
     response = request.form['response']
-    if response == mydict[glb_q]:
+    if response == mydict:
         result = True
     else:
         result = False
-    return render_template('testapp/TorF.html', result = result)
+    return render_template('testapp/TorF.html', result=result)
